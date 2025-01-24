16f25e28f9dc7f254a7a55a490cb33c014ec2efa
Zulhan <zulhanfadil@gmail.com>
2025-01-24 09:08:22 +0700

    feat: add custom web components for header, footer, and form

---
+++ src/custom-element.js
@@ -0,0 +1,61 @@
+class customHeader extends HTMLElement {
+  static observedAttributes = ["title"];
+
+  constructor() {
+    super();
+    this.innerHTML = `
+      <header>
+        <h1>${this.getAttribute("title")}</h1>
+      </header>
+    `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "title") {
+      this.querySelector("h1").textContent = newValue;
+    }
+  }
+}
+
+class customFooter extends HTMLElement {
+  static observedAttributes = ["title"];
+
+  constructor() {
+    super();
+    this.innerHTML = `
+      <footer>
+        <h1>${this.getAttribute("title")}</h1>
+      </footer>
+    `;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "title") {
+      this.querySelector("h1").textContent = newValue;
+    }
+  }
+}
+
+class customForm extends HTMLElement {
+  static observedAttributes = ["title"];
+  constructor() {
+    super();
+    this.innerHTML = `
+      <form id="form">
+        <h1>${this.getAttribute("title")}</h1>
+        <input type="text" name="Judul" placeholder="Title" required>
+        <textarea name="body" placeholder="Isi..." required></textarea>
+        <button type="submit">TAMBAHKAN</button>
+      </form>
+    `;
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "title") {
+      this.querySelector("h1").textContent = newValue;
+    }
+  }
+}
+
+customElements.define("custom-header", customHeader);
+customElements.define("custom-footer", customFooter);
+customElements.define("custom-form", customForm);
